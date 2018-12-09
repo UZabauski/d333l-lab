2f63abb9c9b5e11c6e1e3eed9fdcdad4d8391815
iharkazlouski <tamplierigor@gmail.com>
2018-12-09 23:58:10 +0300

    build steps

--- jobs.groovy
+++ jobs.groovy
@@ -1,13 +1,39 @@
-job("MNTLAB-ikazlouski-main-build-job") {
+job("${SLAVE}/MNTLAB-ikazlouski-main-build-job") {
 	scm {
 		github('MNT-Lab/d333l-lab', 'ikazlouski')
 	}
 	parameters {
-       	 activeChoiceReactiveParam('BUILDS_TRIGGER') {
+           choiceParam('BRANCH_NAME', ['ikazlouski', 'master'])
+        }
+
+        parameters {
+	 activeChoiceReactiveParam('BUILDS_TRIGGER') {
           description('Allows user choose from multiple choices')
           choiceType('CHECKBOX')
           groovyScript {
             script('def used_jobs = ["MNTLAB-ikazlouski-child1-build-job", "MNTLAB-ikazlouski-child2-build-job", "MNTLAB-ikazlouski-child3-build-job", "MNTLAB-ikazlouski-child4-build-job"] \n return used_jobs')
           }
-}
-}
+	 }
+	 
+       }
+	steps
+        {
+            downstreamParameterized
+            {
+                trigger('$BUILDS_TRIGGER') 
+                {
+			
+                    parameters
+                    {
+                        predefinedProp('BRANCH_NAME', '$BRANCH_NAME');
+                    }
+		    block 
+		    {
+	                buildStepFailure('FAILURE');
+			failure('FAILURE');
+			unstable('UNSTABLE');
+		    }
+                }
+            }
+        } 
+} 	
---
+++ script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Hello from $BRANCH_NAME branch"

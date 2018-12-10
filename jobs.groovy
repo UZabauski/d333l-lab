741c2cc636689f62bbf94602161fdb1b61bf8b50
Aliaslol <isachenko.tolik@gmail.com>
2018-12-10 14:31:26 +0300

    add multichoise

--- jobs.groovy
+++ jobs.groovy
@@ -3,7 +3,14 @@ job('MNTLAB-aisachanka-main-build-job') {
 		git('https://github.com/MNT-Lab/d333l-lab.git')
 	}
 	parameters {
-	choiceParam('BRANCH_NAME', ['aisachanka', 'master'])
+		choiceParam('BRANCH_NAME', ['aisachanka', 'master'])
+		activeChoiceParam('child-jobs') {
+        	    description('Allows user choose child jobs')
+	            choiceType('CHECKBOX')
+	            groovyScript {
+	                script('List<String> list = new ArrayList<String>(); for (i = 1; i <5; i++) { list.add("MNTLAB-aisachanka-child$i-build-job") } ; return list')
+	            }
+	        }
 	}
 }
 

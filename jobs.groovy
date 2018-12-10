7c7dbcac9cffc42292f995d307ce9af2460f8034
AndreiSkorkin <skorkin.andrei@gmail.com>
2018-12-10 07:43:21 -0500

    add params

--- jobs.groovy
+++ jobs.groovy
@@ -3,6 +3,18 @@ job("MNTLAB-askorkin-main-build-job") {
 scm {
         github('MNT-Lab/d333l-lab', 'askorkin')
     }
+  
+parameters {
+	choiceParam('BRANCH_NAME', ['askorkin', 'master'], 'Select appropriate branch')
+        activeChoiceParam('JOB') {
+            description('Select jobs')
+            choiceType('CHECKBOX')
+            groovyScript {
+                script('["MNTLAB-askorkin-child1-build-job", "MNTLAB-askorkin-child2-build-job", "MNTLAB-askorkin-child3-build-job", "MNTLAB-askorkin-child4-build-job"]')
+            }
+        }
+}
+        
 
 steps {
 

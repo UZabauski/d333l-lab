042730b9a59fd0c35f609f9599f06a4710c9385e
Aliaslol <isachenko.tolik@gmail.com>
2018-12-10 13:29:24 +0300

    add choose  BRANCH_NAME

--- jobs.groovy
+++ jobs.groovy
@@ -1,5 +1,9 @@
 job('MNTLAB-aisachanka-main-build-job') {
-    scm {
-        git('https://github.com/MNT-Lab/d333l-lab.git')
-    }
+	scm {
+		git('https://github.com/MNT-Lab/d333l-lab.git')
+	}
+	parameters {
+	choiceParam('BRANCH_NAME', ['aisachanka', 'master'])
+	}
 }
+

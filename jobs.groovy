abc2dc5569d60f86e8e4d86e06e71660074c696b
Aliaslol <isachenko.tolik@gmail.com>
2018-12-10 16:27:01 +0300

    add downstream job

--- jobs.groovy
+++ jobs.groovy
@@ -1,16 +1,30 @@
 job('MNTLAB-aisachanka-main-build-job') {
 	scm {
-		git('https://github.com/MNT-Lab/d333l-lab.git')
-	}
-	parameters {
-		choiceParam('BRANCH_NAME', ['aisachanka', 'master'])
-		activeChoiceParam('child-jobs') {
-        	    description('Allows user choose child jobs')
-	            choiceType('CHECKBOX')
-	            groovyScript {
-	                script('List<String> list = new ArrayList<String>(); for (i = 1; i <5; i++) { list.add("MNTLAB-aisachanka-child$i-build-job") } ; return list')
-	            }
-	        }
+		git('https://github.com/MNT-Lab/d333l-lab.git', '$BRANCH_NAME')
+        }
+        parameters {
+                choiceParam('BRANCH_NAME', ['aisachanka', 'master'])
+                activeChoiceParam('child_jobs') {
+                    description('Allows user choose child jobs')
+                    choiceType('CHECKBOX')
+                    groovyScript {
+                        script('List<String> list = new ArrayList<String>(); for (i = 1; i <5; i++) { list.add("MNTLAB-aisachanka-child$i-build-job") } ; return list')
+                    }
+                }
+        }
+	steps {
+		downstreamParameterized {
+			trigger('$child_jobs') { 
+				parameters {
+					predefinedProp('BRANCH_NAME', '$BRANCH_NAME');
+                    		}
+		    	block {
+      				buildStepFailure('FAILURE');
+				failure('FAILURE');
+				unstable('UNSTABLE');
+		    	}
+                	}
+            	}
 	}
 }
 

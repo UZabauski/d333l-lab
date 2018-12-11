5776b632c6374d175acabeb47db1b87589d95bb3
Mclover45 <44427932+Mclover45@users.noreply.github.com>
2018-12-11 14:08:44 +0300

    Create jobs.groovy

---
+++ jobs.groovy
@@ -0,0 +1,65 @@
+us = 'rbachakrou'
+
+
+    job("MNTLAB-$us-main-build-job") {
+        scm {
+            github('MNT-Lab/d333l-lab', "$us")
+        }
+        parameters {
+            choiceParam('BRANCH_NAME', ["$us", 'master'])
+            activeChoiceParam('JOB') {
+                description('Select child jobs')
+                choiceType('CHECKBOX')
+                groovyScript {
+                    script('us = "rbachakrou"; List<String> list = new ArrayList<String>(); for (i = 1; i <5; i++) { list.add("MNTLAB-$us-child$i-build-job") } ; return list')
+                }
+            }
+        }
+        steps {
+            downstreamParameterized {
+                trigger('$JOB') {
+                    block {
+                        buildStepFailure('FAILURE')
+                        failure('FAILURE')
+                        unstable('UNSTABLE')
+                    }
+                    parameters {
+                        predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
+                }                 
+               
+            }
+            
+        }
+
+}   
+   for(i in 1..4) {
+    job("MNTLAB-$us-child${i}-build-job") {
+        scm {
+            github('MNT-Lab/d333l-lab', "$us")
+        }
+        parameters {
+           activeChoiceParam('Branch') {
+          groovyScript {
+              script('''
+                     def gitURL = "https://github.com/MNT-Lab/d333l-lab.git"
+    def command = "git ls-remote -h $gitURL"
+    def proc = command.execute()
+    proc.waitFor()              
+    
+    def branches = proc.in.text.readLines().collect { 
+        it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '') 
+    }
+    return branches ''')
+               }  
+            
+               
+       steps {
+            shell('chmod +x script.sh')
+            shell('./script.sh > output.txt')
+            shell('tar -cvf ${us}_dsl_script.tar.gz output.txt script.sh') 
+        }
+   }
+} 
+}    
+} 
+}

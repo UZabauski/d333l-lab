8dd020b58978cde45fab4cdad95b96403766bc93
uzabauski <uzabauski@gmail.com>
2018-12-11 20:58:16 +0300

    Create main job

--- jobs.groovy
+++ jobs.groovy
@@ -1,1 +1,19 @@
+job("MNTLAB-uzabauski-main-build-job") {
+	scm {
+		github('https://github.com/MNT-Lab/d333l-lab.git', 'uzabauski')
+	}
+	parameters {
+		choiceParam('BRANCH_NAME', ['uzabauski', 'master'], 'Select appropriate branch')
+		activeChoiceParam('jobs') {
+			description('Select jobs')
+			choiceType('CHECKBOX')
+			groovyScript {
+				script('''a = []
+						  for(i in 1..4) {
+						  	  a.add("MNTLAB-uzabauski-child${i}-build-job" )
+						  }
+						  return a''')
+			}
+		}
+	}
 

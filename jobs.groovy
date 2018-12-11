910858bf09ebb7e99125a79a50f825d789bfb2a8
Mclover45 <44427932+Mclover45@users.noreply.github.com>
2018-12-11 20:51:05 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -38,7 +38,7 @@ us = 'rbachakrou'
             github('MNT-Lab/d333l-lab', "$us")
         }
         parameters {
-           choiceParam('BRANCH_NAME', ["$us", 'master'])
+           activeChoiceParam('Branch')
 		{
           groovyScript {
               script('''

960f276e9fb58545dd5d7c550557f10a4434c3cf
uzabauski <uzabauski@gmail.com>
2018-12-11 21:34:11 +0300

    Edit child git 1.2. Test

--- jobs.groovy
+++ jobs.groovy
@@ -44,11 +44,11 @@ for(i in 1..4) {
         steps {
             shell('chmod +x script.sh')
             shell('./script.sh > output.txt')
-            shell('tar -cvf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy output.txt script.sh') 
+            shell('tar -cvf uzabauski_dsl_script.tar.gz jobs.groovy output.txt script.sh') 
         }
         publishers { 
             archiveArtifacts('output.txt')
-            archiveArtifacts('${BRANCH_NAME}_dsl_script.tar.gz')
+            archiveArtifacts('uzabauski_dsl_script.tar.gz')
         }
 	}
 }
